1dc0202aae292f74f690f93de611ee901508fb3d
ChookSF <thomas.charles.griffin@gmail.com>
2014-02-06 21:11:50 -0800

    Drawing a JFrame and putting my Griffin Logo in the frame. The purpose of this is to practice an update from the first hello world version of the program to something that actually has some art in it.

--- Snakebite/src/CodersClub/Projects/Snakebite.java
+++ Snakebite/src/CodersClub/Projects/Snakebite.java
@@ -1,10 +1,48 @@
 package CodersClub.Projects;
 
-public class Snakebite {
+import java.awt.*;
+import java.util.*;
+import java.net.*;
+import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
+public class Snakebite extends JFrame
+{
+    private Image image;
+    
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello Coder Nerds!");
+		new Snakebite();
+		
+	}
+	
+	public Snakebite()
+	{
+		super("Snakebite Game");
+		setSize(1000,800);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		image = tk.getImage(getURL("griffin_logo_noBG.png"));
+	}
+	
+	private URL getURL(String filename)
+	{
+		URL url = null;
+		try {
+			url = this.getClass().getResource(filename);
+		}
+		catch (Exception e) {}
+		return url;
 	}
 
+	public void paint(Graphics g)
+	{
+		Graphics2D  g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
+		g2d.fillRect(0, 0, getSize().width, getSize().height);
+		
+		g2d.drawImage(image, 0, 40, this);
+		
+	}
 }
